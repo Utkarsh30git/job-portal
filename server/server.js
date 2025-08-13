e41fcb2914111134a304ecb2e65ebb8f3582e237
Utkarsh <ut12coc@gmail.com>
2025-08-13 15:23:13 +0530

    userController

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import companyRoutes from './routes/companyRoutes.js';
 import connectCloudinary from './config/cloudinary.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
 import jobRoutes from './routes/jobRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import {clerkMiddleware} from '@clerk/express';
 // Initialise Express
 const app = express();
 
@@ -19,6 +21,7 @@ await connectCloudinary();
 //Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 //Routes
 app.get('/', (req, res) => {
@@ -30,9 +33,9 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 });
 
 app.post('/webhooks',clerkWebhooks)
-
 app.use('/api/company', companyRoutes);
 app.use('/api/jobs', jobRoutes);
+app.use('/api/user', userRoutes);
 
 
 // port
