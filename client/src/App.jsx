a9426519ba275bea9715c27f99e914b7d60c2ca3
Utkarsh <ut12coc@gmail.com>
2025-09-03 23:13:31 +0530

    l2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,35 +9,32 @@ import { AppContext } from './context/AppContext'
 import Dashboard from './pages/Dashboard'
 import AddJob from './pages/AddJob'
 import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'   
+import Home from './pages/Home'                           
 
-import Home from './pages/home'
 import 'quill/dist/quill.snow.css'
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-
-  const {showRecruiterLogin,companyToken}= useContext(AppContext)
+  const { showRecruiterLogin, companyToken } = useContext(AppContext)
 
   return (
     <div>
-      { showRecruiterLogin && <RecruiterLogin />}
+      {showRecruiterLogin && <RecruiterLogin />}
       <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
-        <Route path='/dashboard' element ={<Dashboard/>} >
-         {
-         companyToken ? <>
-         <Route path='add-job' element={<AddJob />} />
-          <Route path='manage-jobs' element={<ManageJobs />} />
-         <Route path='view-applications' element={<ViewApplications />} />
-
-         </> : null
-         }
-          
+        <Route path='/dashboard' element={<Dashboard />}>
+          {companyToken ? (
+            <>
+              <Route path='add-job' element={<AddJob />} />
+              <Route path='manage-jobs' element={<ManageJobs />} />
+              <Route path='view-applications' element={<ViewApplications />} />
+            </>
+          ) : null}
         </Route>
-
       </Routes>
     </div>
   )
